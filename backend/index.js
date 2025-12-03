d27cbe60762f88b7317dbe7045fdc89ace71b2a8
Shahzaib Asif <shahzaibasifjatala@gmail.com>
2025-12-03 16:03:06 +0500

    Refactor CORS setup and improve server initialization

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,54 @@
-
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from 'cors'
-import Stripe from "stripe";
+import cors from 'cors';
 
-// local imports
 import connectDB from "./config/connectdb.js";
 import { authRouter } from "./routes/authroute.js";
 import { userRoute } from "./routes/userRoute.js";
 import { courseRoute } from "./routes/courseRoute.js";
 import paymentRoute from "./routes/paymentroute.js";
 
-
 const app = express();
 dotenv.config();
-app.use(express.json())
-app.use(cookieParser())
+
+connectDB(); 
+
+app.use(express.json());
+app.use(cookieParser());
+
+const allowedOrigins = [
+  'https://lmsforstudents.netlify.app',
+  'https://lms-lime-delta.vercel.app',
+  'http://localhost:5173' 
+];
 
 app.use(cors({
-   origin:'https://lmsforstudents.netlify.app',
-   credentials:true
-}))
-
+   origin: function (origin, callback) {
+     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+       callback(null, true);
+     } else {
+       callback(new Error('Not allowed by CORS'));
+     }
+   },
+   credentials: true
+}));
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRoute);
 app.use("/api/course", courseRoute);
 app.use("/api/payment", paymentRoute);
 
-// app.get('/', (req,res,next) => {
-//    res.send("hello world");
-// })
+app.get('/', (req, res) => {
+    res.send("API is running...");
+});
 
-const port = process.env.PORT;
-app.listen(port, () => {
-    connectDB();
-    console.log("Server is running")
-})
+const port = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}
+
+export default app;
